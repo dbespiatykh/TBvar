49a2b7e84edf8a0053bbbfd831c2d54473423b41
Dmitry Bespiatykh <55133346+dbespiatykh@users.noreply.github.com>
2021-10-05 09:22:13 +0300

    Update rtg_concat.py

--- scripts/rtg_concat.py
+++ scripts/rtg_concat.py
@@ -1,23 +1,11 @@
-# %%
 import os
 import pandas as pd
 
-# %%
 with open(snakemake.input[0], 'r') as file:
     data = file.read().split('\n\n')
 data = [i.split('\n') for i in data]
 
-# %%
 df = pd.DataFrame(data)
-
-df=df[[1,2]]
-
-df.rename(columns={1: "SampleName", 2: "SNPs"}, inplace=True)
-
-# %%
-df = df[['SampleName', 'SNPs']]
-
-# %%
+df=df[[0,1]]
+df.rename(columns={0: "SampleName", 1: "SNPs"}, inplace=True)
 df.to_csv(snakemake.output[0], sep='\t', index=False)
-
-# %%
